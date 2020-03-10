083fb1f5c9fd29671173087ec62eeade6ee71173
hagooog <mohamed.higgy31@gmail.com>
2020-03-10 04:37:09 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,83 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-3">
+        <h1 class="text-center">Simple Quiz app</h1>
+      </div>
     </div>
-    <router-view />
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-3">
+        <transition name="flip" mode="out-in">
+          <component
+            :is="mode"
+            @answered="answered($event)"
+            @confirmed="mode = 'app-question'"
+          ></component>
+        </transition>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Question from "./components/Question";
+import Answer from "./components/Answer";
+export default {
+  data() {
+    return {
+      mode: "app-question"
+    };
+  },
+  methods: {
+    answered: function(isCorrect) {
+      if (isCorrect) {
+        this.mode = "app-answer";
+      } else {
+        this.mode = "app-question";
+        alert("Worng, try again!");
+      }
+    }
+  },
+  components: {
+    appQuestion: Question,
+    appAnswer: Answer
+  }
+};
+</script>
+
+<style>
+.flip-enter {
+  /*transform: rotateY(0deg);*/
 }
 
-#nav {
-  padding: 30px;
+.flip-enter-active {
+  animation: flip-in 0.5s ease-out forwards;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.flip-leave {
+  /*transform: rotateY(0deg);*/
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.flip-leave-active {
+  animation: flip-out 0.5s ease-out forwards;
+}
+
+@keyframes flip-out {
+  from {
+    transform: rotateY(0deg);
+  }
+  to {
+    transform: rotateY(90deg);
+  }
+}
+
+@keyframes flip-in {
+  from {
+    transform: rotateY(90deg);
+  }
+  to {
+    transform: rotateY(0deg);
   }
 }
 </style>
